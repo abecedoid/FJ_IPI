58333d040a7a89e5e1655c660a7bd2aa2405e99a
martin-dostal-eli <martin.dostal@eli-laser.eu>
2021-10-17 22:33:41 +0200

    created first fringe count

--- detector/fringe.py
+++ detector/fringe.py
@@ -1,5 +1,6 @@
 import numpy as np
 from helpers.labeled_jsons import DropletLabel
+from detector.circle_detector import find_peaks2d
 
 
 class DropletSlice(object):
@@ -12,9 +13,16 @@ class DropletSlice(object):
     def img(self):
         return self._img
 
+    @img.setter
+    def img(self, img: np.ndarray):
+        self._img = img
+
     @property
     def radius(self):
         return self._radius
+
+    def center_coords(self) -> list:
+        return [int(self._img.shape[0]/2), int(self._img.shape[1]/2)]
 
 
 def get_droplet_slices_from_img(img: np.ndarray, droplet_labels: list) -> list:
@@ -40,6 +48,31 @@ def droplet_slice_from_image(img:np.ndarray, droplet_label: DropletLabel, radius
     return DropletSlice(img_slice, droplet_label.radius(), score=None)
 
 
-def count_fringes(ds: DropletSlice):
-    """Takes the image and estimates number of fringes in it"""
-    raise NotImplementedError
+def dist2points(p1: list, p2: list) -> float:
+    return np.sqrt(((p1[0] - p2[0]) ** 2) + ((p1[1] - p2[1]) ** 2))
+
+
+def count_fringes(ds: DropletSlice, max_min_fltr_size: int=3, peak_thr: float=0.1) \
+        -> (float, np.ndarray, np.ndarray):
+    """Takes the image and estimates number of fringes in it
+    Returns the number of fringes, the FFT(img) and the coords of the FFT(img) peaks (excluding the DC part)"""
+    # todo - if/when some time, solve it for the occasion, when two droplets overlap (multiple maxima in fft and all that jazz
+    # fast fourie it
+    I = np.abs(np.fft.fftshift(np.fft.fft2(ds.img)))
+    # normalize it for thresholding in find 2d peaks
+    I = I / np.max(I[:])
+
+    # find peaks
+    pk_coords = find_peaks2d(arr=I, max_min_neighborhood_size=max_min_fltr_size, threshold=peak_thr)
+
+    # remove the center DC coordinate
+    try:
+        # find row with the center index
+        row_idx = np.where((pk_coords == ds.center_coords()).all(axis=1))[0][0]
+        np.delete(pk_coords, row_idx, axis=0)
+    except Exception as e:
+        print('for some reason the center was not a part of detected peaks: {}'.format(e))
+
+    # compute the distance between center and peak
+    dist = dist2points(ds.center_coords(), pk_coords[0])
+    return dist, I, pk_coords
---
+++ evaluation/test_fringe_counting.py
@@ -0,0 +1,46 @@
+import os
+import numpy as np
+from helpers.labeled_jsons import *
+import cv2
+from detector.fringe import *
+import matplotlib.pyplot as plt
+from detector.circle_detector import find_peaks2d
+
+
+if __name__ == '__main__':
+
+    DIRPATH = '../resources/105mm_60deg.6mxcodhz.000000'
+    DIRPATH = os.path.abspath(DIRPATH)
+    imfnames = os.listdir(DIRPATH)
+
+    for imfname in imfnames:
+
+        impath = os.path.join(DIRPATH, imfname)
+
+        # load numpy image and ground truth droplets
+        img = load_labelme_image(impath)
+        gt_droplets = load_labelme_droplet_labels(impath)
+
+        for gt_droplet in gt_droplets:
+            ds = droplet_slice_from_image(img, gt_droplet)
+
+            # random rotation
+            for i in range(int(np.round(np.random.random() * 3))):
+                ds.img = np.rot90(ds.img)
+
+            n, DS, pk_coords = count_fringes(ds)
+            print('fringe count is: {}'.format(n))
+
+            plt.figure()
+            plt.subplot(131)
+            plt.imshow(ds.img)
+            plt.subplot(132)
+            plt.imshow(DS)
+            plt.subplot(133)
+            plt.imshow(DS)
+            plt.scatter(pk_coords[:, 0], pk_coords[:, 1], color='r', s=1)
+            plt.show()
+
+
+
+
